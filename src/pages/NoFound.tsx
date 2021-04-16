f28479f4a4a4972e9ec8c761ba1be368d9b9656b
unknown <dabrowskidaniel006@gmail.com>
2021-04-16 17:41:41 +0200

    404 page

--- src/pages/NoFound.tsx
+++ src/pages/NoFound.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Helmet } from "react-helmet";
+
+const Container = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    flex: 1;
+`
+
+const Message = styled.span`
+    font-family: ${({theme}) => theme.primaryFont};
+    color: ${({theme}) => theme.fontColor};
+    font-size: 3em;
+    user-select: none;
+`
 
 const NoFound = () => {
     return (
-        <div>
-            <h1>404</h1>
-        </div>
+        <Container>
+            <Helmet>
+                <title>404 - Vocupractice</title>
+            </Helmet>
+            <Message>Hey! It looks like you're lost!</Message>
+            <Message>Click on vocupractice logo to go back :)</Message>
+        </Container>
     );
 };
 
